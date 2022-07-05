deb05a9588e78d2933fed784a82e6176e8264495
jiren <yujienjoyfever@yahoo.co.jp>
2022-07-05 18:02:48 +0900

    delete comment

--- src/com/volkruss/cider/Cider.java
+++ src/com/volkruss/cider/Cider.java
@@ -17,7 +17,6 @@ public class Cider {
 		this.converter = new Converter();
 	}
 	
-
 	/**
 	 * 
 	 * キャメルケースで宣言されているモデルからスネークケースのモデルを取得します
@@ -46,31 +45,25 @@ public class Cider {
 		return this.getModelWithConvertLogic(to, from, this.converter::toCamel, exclusions);
 	}
 	
-	
 	public <T,U> T toModel(Class<T> to, U from, String ...exclusions) {
 		return this.getModelWithConvertLogic(to, from, i -> i, exclusions);
 	}
-	
-	
 	
 	private <T,U> T getModelWithConvertLogic(Class<T> to ,U from, UnaryOperator<String> func,String ...exclusions) {
 		T ins = null;
 		try {
 			Constructor<T> con = to.getDeclaredConstructor();
 			ins = (T)con.newInstance();
-			// privateも含めて全て取得する
 			for(Field field : from.getClass().getDeclaredFields()) {
 				field.setAccessible(true);
-				// fromがsample_data(スネークケース)の形になっているのでsampleData(キャメルケース)に変換する
 				String toName = func.apply(field.getName());		
-				//　チェックする
+				
 				if(!this.check(to.getDeclaredFields(),toName,field.getName(),exclusions)) {
 					continue;
 				}
-				// マッピング先のクラスの変数を取得する
+				
 				Field toField = to.getDeclaredField(toName);
 				toField.setAccessible(true);
-				// マッピング先のクラスに、元のフィールドの値を設定する
 				toField.set(ins, field.get(from));
 			}	
 		} catch (Exception e) { System.out.println(e); }
@@ -85,12 +78,10 @@ public class Cider {
 			toFieldStrs.add(field.getName());
 		}
 		
-		// フィールド未存在チェック
 		if(!toFieldStrs.contains(toName)) {
 			return false;
 		}
 		
-		// 除外項目チェック
 		if(Arrays.asList(exclusions).contains(fromName)) {
 			return false;
 		}
